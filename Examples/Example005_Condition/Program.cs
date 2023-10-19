be6ff4a9bf6eb574d558d67daed0158b3c28a719
Merkhat <maliqmerkhat@gmail.com>
2023-10-19 16:03:44 +0600

    add Example 005 Condition

---
+++ Examples/Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "erke")
+{
+    Console.WriteLine("Hooray, it's Erke!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
